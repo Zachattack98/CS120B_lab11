04fde5be4dcd5c545b1a736e40b33d1caba83a73
Jia Xie <jxie031@wch136-16.cs.ucr.edu>
2019-07-16 15:11:47 -0700

    ..

--- Lab11_taskScheduler/turnin/part2.c
+++ Lab11_taskScheduler/turnin/part2.c
@@ -24,20 +24,20 @@ unsigned char string[37]="CS120B is Legend... wait for it DARY!";
 unsigned char string_length=37;
 unsigned char pointer = 0;
 unsigned char i = 0;
-unsigned char display_string[16] = "";
+unsigned char display_string[16];
 //Monitors button connected to PA0.
 //When button is pressed, shared variable "pause" is toggled.
-enum tickStates{start,Lessthan16, Greaterthan16, Greaterthan36};
-int tick(int state)
+enum tickStates{start,Lessthan16, Greaterthan16};
+unsigned char tick(int state)
 {
+	
+	display_string[16]="";
 	
 	switch(state)
 	{
 		case start: state = Lessthan16;break;
 		case Lessthan16: state = (pointer>15)? Greaterthan16: Lessthan16; break;
-		case Greaterthan16: state = (pointer >= 36)? Greaterthan36: Greaterthan16;break;
-		case Greaterthan36: state = (pointer >= 52)? start: Greaterthan36;break;
-		default: state = start;break;
+		case Greaterthan16: state = (pointer == 36)? state = start: Greaterthan16;break;
 	}
 	switch(state)
 	{
@@ -46,76 +46,38 @@ int tick(int state)
 			pointer = 0;
 			break;
 		case Lessthan16:
-			for(i = 0; i < pointer+1; i++)
+			for(i = 0; i < pointer; i++)
 			{
 				display_string[15-pointer+i] = string[i];
 			}
 			for(i = 0; i < 15- pointer;i++)
 			{
-				display_string[i] = ' ';
+				display_string[i] = " ";
 			}
 			pointer++;
 			break;
 		case Greaterthan16:
-			for(i = 0; i < 16; i++)
+			for(i = 0; i < pointer; i++)
 			{
-				display_string[i] = string[pointer-15+i];
-			}
-			pointer++;
-			break;
-		case Greaterthan36:
-			for(i = 0; i < 52-pointer+1; i++)
-			{
-				display_string[i] = string[pointer-16+i];
-			}
-			for(i++; i < 16; i++)
-			{
-				display_string[i] = ' ';
+				display_string[pointer-16+i] = string[i];
 			}
 			pointer++;
 			break;
 		default:
 			break;
 	}
-	return state;
 }
 
-enum displayStates{start1,display};
-int displaySM(int state)
-{
-	
-	switch(state)
-	{
-		case start1:
-			state = display;
-			break;
-		case display:
-			break;
-		default:
-			state = start;
-			break;
-	}
-	switch(state)
-	{
-		case start1:
-			break;
-		case display:
-			
-			LCD_DisplayString(1, display_string);
-			break;
-	}
-	return state;
-}
+enum displayStates{start,display};
+
 int main(void) {
     /* Insert DDR and PORT initializations */
 	/*unsigned char x;
 	DDRB = 0xFF; PORTB = 0x00;*/
 	DDRC = 0xFF; PORTC  = 0x00;
-	DDRD = 0xFF; PORTD = 0x00;
 //	DDRA = 0x00; PORTA = 0xFF;
 //	DDRB = 0xFF; PORTB = 0x00;
 	LCD_init();
-	//LCD_DisplayString(1,string);
     /* Insert your solution below */
     static task task1, task2;
     task *tasks[] = {&task1, &task2};
@@ -123,16 +85,11 @@ int main(void) {
     
     //Task 1 (pauseButtonToggleSM)
     
-    task1.state = start;//Task initial state
+    task1.state = pauseButton_wait;//Task initial state
     task1.period = 1;//Task Period
     task1.elapsedTime = task1.period;//Task current elapsed time.
-    task1.TickFct = &tick;//Function pointer for the tick
+    task1.TickFct = &pauseButtonSMTick;//Function pointer for the tick
     //Set the timer and turn it on
-    
-    task2.state = start1;//Task initial state
-    task2.period = 1;//Task Period
-    task2.elapsedTime = task2.period;//Task current elapsed time.
-    task2.TickFct = &displaySM;//Function pointer for the tick
 	unsigned long int GCD = 1;
     TimerSet(1);
     TimerOn();
